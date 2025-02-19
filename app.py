f4482481217bec71826bb1e20c35a7d558d34de1
BervinBekSchool <sutdbervin@gmail.com>
2025-02-19 23:57:28 +0800

    first_commit

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ from google import genai
 # st.set_page_config(page_title="Bervin", page_icon=":up:", layout="centered", initial_sidebar_state="auto", menu_items=None) 
 
 st.title("Why are you not doing work?\nBy Bervin")
-# client = genai.Client(api_key="")
+client = genai.Client(api_key="AIzaSyAKqHDLoSN1Rs9yvrCOlJO2bPQXBhLNook")
 
-# ### Generate a pre-defined prompt ###
-# response = client.models.generate_content(
-#     model='gemini-2.0-flash', 
-#     contents='Tell me current affairs 8 words.'
-# )
-# st.write(response.text)
+### Generate a pre-defined prompt ###
+response = client.models.generate_content(
+    model='gemini-2.0-flash', 
+    contents='Tell me 1 fun fact about gemini ai in 8 words.'
+)
+st.write(response.text)
 
 
-# ### Generate a pre-defined prompt ###
-# qns = st.text_input(label="Ask Gemini Questions")
+### Generate a pre-defined prompt ###
+qns = st.text_input(label="Ask Gemini Questions")
 
-# if st.button("Submit"):
-#     response2 = client.models.generate_content(
-#     model='gemini-2.0-flash', 
-#     contents=qns
-#     )
-#     st.write(response2.text)
+if st.button("Submit"):
+    response2 = client.models.generate_content(
+    model='gemini-2.0-flash', 
+    contents=qns
+    )
+    st.write(response2.text)
 
